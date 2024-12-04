05f7f0db28775d8cb6578bcfd8bc9eb61f443fc2
Joshua Oladeji <dejijolaoluwa@gmail.com>
2024-12-04 12:55:44 +0100

    wip: read and compile huff file

---
+++ README.md
@@ -0,0 +1,2 @@
+# Stack-rs
+a tool to visualize the stack, memory and calldata when writing smart-contracts in evm low-level language - huff
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,46 @@
+use huff_core::Compiler;
+use huff_utils::evm_version::EVMVersion;
+use std::{
+    io::{self, Write},
+    sync::Arc,
+};
+
 fn main() {
-    println!("Hello, world!");
+    let mut sources: Vec<String> = Vec::new();
+    let evm_version = EVMVersion::default();
+    let mut file_path = String::new();
+
+    print!("source: ");
+    io::stdout().flush().unwrap();
+
+    io::stdin()
+        .read_line(&mut file_path)
+        .expect("error: file read");
+    println!("file path: {}", file_path);
+
+    sources.push(file_path);
+
+    // vec!["./playground.huff".to_string()]
+    let compiler = Compiler::new(
+        &evm_version,      // EVM version
+        Arc::new(sources), // sources - file paths
+        None,              // output - file path
+        None,              // alternative `MAIN()` macro
+        None,              // alternative `CONSTRUCTOR()` macro
+        None,              // constructor arguments - for the generation of runtime code
+        None,              // constant overrides
+        false,             // verbose ??
+        false,             // check cached artifacts ??
+    );
+
+    let result = compiler.execute();
+    match result {
+        Ok(artifact) => {
+            println!("bytecode: {}", artifact[0].bytecode);
+        }
+
+        Err(error) => {
+            println!("Error: {:?}", error);
+        }
+    }
 }
